73f94ec28d87a5ffcb2e51ba1ea0eb12bbabf02a
vauzi <ismanvauzi1998@gmail.com>
2022-11-20 23:29:36 -1200

    menambahkan section ke 6 dan responsive mobile

--- assets/js/scoll.js
+++ assets/js/scoll.js
@@ -1,20 +1,22 @@
 const nav = document.getElementById('nav-link');
+const navi = document.getElementById('navbar');
 
 function toggle() {
-  nav.classList.toggle('translate-x-44');
+  //   nav.classList.toggle('translate-x-44');
   nav.classList.toggle('opacity-0');
+  navi.classList.toggle('bg-gray-600');
 }
 
 window.addEventListener('scroll', () => {
   var nav = document.querySelectorAll('.navbar');
 
   for (var i = 0; i < nav.length; i++) {
-    if (window.scrollY > 0) {
-      nav[i].classList.remove('lg:bg-transparent');
-      nav[i].classList.add('px-7');
+    if (window.scrollY > 100) {
+      nav[i].classList.add('bg-gray-600');
+      nav[i].classList.add('px-5');
     } else {
-      nav[i].classList.remove('px-7');
-      nav[i].classList.add('lg:bg-transparent');
+      nav[i].classList.remove('bg-gray-600');
+      nav[i].classList.remove('px-5');
     }
   }
 });
